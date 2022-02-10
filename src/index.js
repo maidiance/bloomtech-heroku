8826a6763a22e2136f4d24aa93fb392dff0a503c
Winifred Lu <winnie.lu@comcast.net>
2022-02-10 13:22:15 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+    console.log(process.env.MESSAGE);
+});
+
+server.use('*', (req, res) => {
+    res.send('<h1>Hello, Heroku!</h1>');
+});
